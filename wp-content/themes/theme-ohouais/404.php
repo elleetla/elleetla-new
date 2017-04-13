8cf18fdfb8712df7bfe9c6286a55bdc85a6882ef
elleetla <elleetla@gmail.com>
2017-04-13 16:41:18 +0200

    redesign 404.php

--- wp-content/themes/theme-ohouais/404.php
+++ wp-content/themes/theme-ohouais/404.php
@@ -1,1 +1,19 @@
-404
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-xs-12 col-sm-12 col-lg-12 col-md-12">
+            <div id="content" role="main">
+                <div id="content_404">
+                    <h1>404</h1>
+                    <p>Oops, Cette page n'a pas pu être trouvée !</p>
+                </div>
+            </div><!-- /#content -->
+        </div>
+
+    </div><!-- /.row -->
+</div><!-- /.container -->
+
+
+<?php get_footer(); ?>
