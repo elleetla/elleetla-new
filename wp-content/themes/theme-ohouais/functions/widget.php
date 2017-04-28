46659830cebf7c342c5ac889634a3dabdac1d48b
elleetla <elleetla@gmail.com>
2017-04-28 11:40:17 +0200

    clean functions/widget.php / remove un-use widget (sidebar-right and sidebar-projet)

--- wp-content/themes/theme-ohouais/functions/widget.php
+++ wp-content/themes/theme-ohouais/functions/widget.php
@@ -1,83 +1,49 @@
-<?php 
+<?php
+/**
+* Creates a sidebar
+* @param string|array  Builds Sidebar based off of 'name' and 'id' values.
+*/
 
-	   /**
-		* Creates a sidebar
-		* @param string|array  Builds Sidebar based off of 'name' and 'id' values.
-		*/
+function widget_ohouais(){
+    // footer
+    $args = array(
+        'name'          => __( 'Footer principal', 'theme_text_domain' ),
+        'id'            => 'sidebar-footer',
+        'description'   => 'Cette sidebar permet d\'étiter le footer principal du site',
+        'class'         => '',
+        'before_widget' => '<li id="mentions-footer" class="widget">',
+        'after_widget'  => '</li>',
+        'before_title'  => '<h4 class="widgettitle">',
+        'after_title'   => '</h4>'
+    );
+    register_sidebar( $args );
 
-		function widget_ohouais(){
+    //slidehome
+    $args = array(
+        'name'          => __( 'Slide home header', 'theme_text_domain' ),
+        'id'            => 'slidehome',
+        'description'   => 'Cette sidebar permet d\'étiter le slide de la page d\'accueil du site',
+        'class'         => '',
+        'before_widget' => '<div id="slide-home" class="widget">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="widgettitle">',
+        'after_title'   => '</h4>'
+    );
+    register_sidebar( $args );
 
-			// colonne right
-			$args = array( 
-				'name'          => __( 'Sidebar right', 'theme_text_domain' ),
-				'id'            => 'sidebar-right',
-				'description'   => 'Cette sidebar permet d\'étiter la colonne de droite du site',
-				'class'         => '',
-				'before_widget' => '<li class="widget-col-right">',
-				'after_widget'  => '</li>',
-				'before_title'  => '<h4 class="widgettitle">',
-				'after_title'   => '</h4>'
-			);
-		
-			register_sidebar( $args );
+    //contactinfos
+    $args = array(
+        'name'          => __( 'Information de contact', 'theme_text_domain' ),
+        'id'            => 'contactinfos',
+        'description'   => 'Cette sidebar permet d\'étiter la partie contact déroulante du site',
+        'class'         => '',
+        'before_widget' => '<div id="detail-contact-listing">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="widgettitle">',
+        'after_title'   => '</h4>'
+    );
+    register_sidebar( $args );
 
-			// footer 
-			$args = array(
-				'name'          => __( 'Footer principal', 'theme_text_domain' ),
-				'id'            => 'sidebar-footer',
-				'description'   => 'Cette sidebar permet d\'étiter le footer principal du site',
-				'class'         => '',
-				'before_widget' => '<li id="mentions-footer" class="widget">',
-				'after_widget'  => '</li>',
-				'before_title'  => '<h4 class="widgettitle">',
-				'after_title'   => '</h4>'
-			);
+}
 
-
-			register_sidebar( $args );
-
-
-			$args = array(
-				'name'          => __( 'Slide home header', 'theme_text_domain' ),
-				'id'            => 'slidehome',
-				'description'   => 'Cette sidebar permet d\'étiter le slide de la page d\'accueil du site',
-				'class'         => '',
-				'before_widget' => '<div id="slide-home" class="widget">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h4 class="widgettitle">',
-				'after_title'   => '</h4>'
-			);
-		
-			register_sidebar( $args );
-
-			$args = array(
-				'name'          => __( 'Information de contact', 'theme_text_domain' ),
-				'id'            => 'contactinfos',
-				'description'   => 'Cette sidebar permet d\'étiter la partie contact déroulante du site',
-				'class'         => '',
-				'before_widget' => '<div id="detail-contact-listing">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h4 class="widgettitle">',
-				'after_title'   => '</h4>'
-			);
-		
-			register_sidebar( $args );
-
-			$args = array(
-				'name'          => __( 'Sidebar projet', 'theme_text_domain' ),
-				'id'            => 'sidebar-projet',
-				'description'   => 'Cette sidebar permet d\'étiter la partie "projet récent"',
-				'class'         => '',
-				'before_widget' => '<div>',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h4 class="widgettitle">',
-				'after_title'   => '</h4>'
-			);
-		
-			register_sidebar( $args );
-
-		}
-
-		add_action('widgets_init','widget_ohouais');
-
- ?>
+add_action('widgets_init','widget_ohouais');
