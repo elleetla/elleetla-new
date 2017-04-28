e23287ee049ea301d93e82c0d1f9024bba9ac868
elleetla <elleetla@gmail.com>
2017-04-28 11:26:31 +0200

    in realisations.php, move jQuery instructions for filtering portfolio by category into js/custom.js

--- wp-content/themes/theme-ohouais/js/custom.js
+++ wp-content/themes/theme-ohouais/js/custom.js
@@ -51,7 +51,55 @@ $(document).ready(function() {
             $('#header-project').removeClass("sticky");
         }
     });
-    
+
+    /* in réalisations, filter projets when click on categoies navbar */
+    var $container = $('#portfolio');
+
+    // create a clone that will be used for measuring container width
+    $containerProxy = $container.clone().empty().css({ visibility: 'hidden' });
+
+    $container.after( $containerProxy );
+
+    // get the first item to use for measuring columnWidth
+    var $item = $container.find('.portfolio-item').eq(0);
+
+    $container.imagesLoaded(function(){
+        $(window).smartresize( function() {
+
+            // calculate columnWidth
+            var colWidth = Math.floor( $containerProxy.width() / 3 ); // Change this number to your desired amount of columns
+
+            // set width of container based on columnWidth
+            $container.css({
+                width: colWidth * 3 // Change this number to your desired amount of columns
+            })
+                .isotope({
+
+                    // disable automatic resizing when window is resized
+                    resizable: false,
+
+                    // set columnWidth option for masonry
+                    masonry: {
+                        columnWidth: colWidth
+                    }
+                });
+
+            // trigger smartresize for first time
+        }).smartresize();
+    });
+
+    // filter items when filter link is clicked
+    $('#filters a').click(function(){
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({ filter: selector, animationEngine : "css" });
+        $(this).addClass('active');
+        $('#filters a.active').removeClass('active');
+        return false;
+
+    });
+    /* in réalisations, filter projets when click on categories navbar */
+
 // Closure javascript sheet elle&la     
 });
 
--- wp-content/themes/theme-ohouais/realisations.php
+++ wp-content/themes/theme-ohouais/realisations.php
@@ -7,39 +7,35 @@ Template Name: Réalisations
 <?php
 get_header(); ?>
  
-    <!-- .container -->
-    <div class="container">
-        <!-- .row -->
-        <div class="row">
-            <!-- .col-lg-12 -->
-            <div class="col-lg-12">
 
-            <nav id="navFiltre">
+    <div class="container"><!-- .container -->
+        <div class="row"><!-- .row -->
+            <div class="col-lg-12"><!-- .col-lg-12 -->
+                <nav id="navFiltre">
 
-                <ul id="filters">
-                <?php
-                    $terms = get_terms("categories");
-                    $count = count($terms);
-                        echo '<li><a href="javascript:void(0)" title="" data-filter=".all" class="active">Tous</a></li><span></span>';
-                    if ( $count > 0 ){
-         
-                        foreach ( $terms as $term ) {
-         
-                            $termname = strtolower($term->name);
-                            $termname = str_replace(' ', '-', $termname);
-                            echo '<li><a href="javascript:void(0)" title="" data-filter=".'.$termname.'">'.$term->name.'</a></li><span></span>';
-                        }
-                    }
-                ?>
-                </ul>
-            </div>
+                    <ul id="filters">
 
-            </nav>
+                        <?php
+                            $terms = get_terms("categories");
+                            $count = count($terms);
+                                echo '<li><a href="javascript:void(0)" title="" data-filter=".all" class="active">Tous</a></li><span></span>';
+                            if ( $count > 0 ){
 
-            <!-- /.col-lg-12 -->
+                                foreach ( $terms as $term ) {
+
+                                    $termname = strtolower($term->name);
+                                    $termname = str_replace(' ', '-', $termname);
+                                    echo '<li><a href="javascript:void(0)" title="" data-filter=".'.$termname.'">'.$term->name.'</a></li><span></span>';
+                                }
+                            }
+                        ?>
+
+                    </ul>
+                </nav>
+            </div><!-- /.col-lg-12 -->
  
-            <!-- /.portfolio -->
-            <div id="portfolio">
+
+            <div id="portfolio"><!-- .portfolio -->
  
                 <?php 
                 $args = array( 'post_type' => 'projet', 'posts_per_page' => -1 );
@@ -59,81 +55,26 @@ get_header(); ?>
                         $tax = strtolower($tax_links);
                     else :    
                     $tax = '';                    
-                    endif; 
-         
-                    echo '<div class="all col-lg-4 portfolio-item '. $tax .'">';
-                    echo '<div class="bloc-projet">';
-                    echo '<a href="'; the_permalink();
-                    echo '">';
-                    echo '<img class="img-responsive" src="'; the_post_thumbnail_url();
-                    echo '"/>';
-                    echo '<div class="hover-project">';
-                    echo '<h1>';the_title();'</h1>';
-                    echo '</div>';
-                    echo '</a>';
-                    echo '</div>';
-                    echo '</div>'; 
+                    endif;
+                ?>
 
-                endwhile; ?>
- 
-            </div><!-- /#portfolio -->
+                <div class="all col-lg-4 portfolio-item <?php echo $tax; ?>"><!-- .all col-lg-4 -->
+                    <div class="bloc-projet"><!-- .bloc-projet -->
+                        <a href="<?php the_permalink();?>">
+                            <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>"/>
 
-        </div>
-        <!-- /.row -->
-   </div>
-   <!-- /.container -->
+                            <div class="hover-project">
+                                <h1><?php the_title(); ?></h1>
+                            </div>
 
+                        </a>
+                    </div><!-- ./bloc-projet -->
+                </div><!-- ./all col-lg-4 -->
 
-   <script>
-   (function($){
+                <?php endwhile; ?>
  
-    var $container = $('#portfolio');
- 
-    // create a clone that will be used for measuring container width
-    $containerProxy = $container.clone().empty().css({ visibility: 'hidden' });   
- 
-    $container.after( $containerProxy );  
- 
-    // get the first item to use for measuring columnWidth
-    var $item = $container.find('.portfolio-item').eq(0);
-    
-    $container.imagesLoaded(function(){
-        $(window).smartresize( function() {
- 
-            // calculate columnWidth
-            var colWidth = Math.floor( $containerProxy.width() / 3 ); // Change this number to your desired amount of columns
- 
-            // set width of container based on columnWidth
-            $container.css({
-                width: colWidth * 3 // Change this number to your desired amount of columns
-            })
-            .isotope({
- 
-                // disable automatic resizing when window is resized
-                resizable: false,
- 
-                // set columnWidth option for masonry
-                masonry: {
-                    columnWidth: colWidth
-                }
-            });
- 
-        // trigger smartresize for first time
-        }).smartresize();
-    });
- 
-    // filter items when filter link is clicked
-    $('#filters a').click(function(){
-        
-        var selector = $(this).attr('data-filter');
-        $container.isotope({ filter: selector, animationEngine : "css" });
-        $(this).addClass('active');
-        $('#filters a.active').removeClass('active');
-        return false;
- 
-    });
- 
-} ) ( jQuery );
-   </script>
- 
+            </div><!-- /.portfolio -->
+        </div><!-- /.row -->
+   </div><!-- /.container -->
+
 <?php get_footer(); ?>
