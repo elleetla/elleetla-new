d728b11eca0b6501552ab92440e9d80fd4fc6d19
elleetla <elleetla@gmail.com>
2017-07-04 18:02:11 +0200

    update metiers page

--- wp-content/themes/theme-ohouais/metiers.php
+++ wp-content/themes/theme-ohouais/metiers.php
@@ -6,13 +6,15 @@ Template Name: Métiers
 get_header();
 
 // Declaration of variables
-$theContent = get_the_content(); ?>
+$theContent = get_the_content();
+$close = get_template_directory_uri().'/images/close-or.png';
+?>
 
     <div class="container">
         <div id="section-profils">
             <div class="row">
                 <div class="col-md-offset-2 col-lg-8">
-                    <div id="intro-metier"><?php echo $theContent; ?></div>
+                    <div id="intro-metier"><?php echo $theContent;?></div>
                 </div>
             </div>
         </div>
@@ -20,12 +22,13 @@ $theContent = get_the_content(); ?>
         <div class="row">
             <div class="const-1"><!-- Déterminer une vision stratégique globale -->
                 <div class="bloc-text">
-                    <h3><?php echo get_field('title_vision_globale'); ?></h3>
+                    <h4><?php echo get_field('title_vision_globale'); ?></h4>
                     <p><?php echo get_field('vision_globale'); ?></p>
 
-                    <div class="plus-infos">
+                    <div class="plus-infos" id="comment2">
+                        <a class="close-infos-comment"><img src="<?php echo $close; ?>" alt="close"></a>
+                        <p class="comment">Comment ?</p>
                         <p><?php echo get_field('comment_vision_globale'); ?></p>
-                        <a class="close-infos-comment">Close</a>
                     </div><!-- ./plus-infos -->
 
                     <a class="btn-infos-comment">Comment ?</a>
@@ -35,7 +38,7 @@ $theContent = get_the_content(); ?>
                     <figure>
                         <?php $image = get_field('membre1'); ?>
                         <img class="img-responsive" src="<?php echo $image['url'];?>" alt="<?php echo $image['title']; ?>">
-                        <figcaption class="name-img"><?php echo $image['caption'] ?></figcaption>
+                        <figcaption class="figcaption caption-right"><?php echo $image['caption'] ?></figcaption>
                     </figure>
                 </div><!-- ./col-md-offset-6 col-lg-6 -->
 
@@ -44,12 +47,13 @@ $theContent = get_the_content(); ?>
 
             <div class="const-1"><!-- Affirmer votre identité en lui donnant du visuel -->
                 <div class="bloc-text-right">
-                    <h3><?php echo get_field('title_affirmer_id'); ?></h3>
+                    <h4><?php echo get_field('title_affirmer_id'); ?></h4>
                     <p><?php echo get_field('affirmer_id'); ?></p>
 
-                    <div class="plus-infos">
+                    <div class="plus-infos" id="comment2">
+                        <a class="close-infos-comment"><img src="<?php echo $close; ?>" alt="close"></a>
+                        <p class="comment">Comment ?</p>
                         <p><?php echo get_field('comment_affirmer_id'); ?></p>
-                        <a class="close-infos-comment">Close</a>
                     </div><!-- ./plus-infos -->
 
                     <a class="btn-infos-comment">Comment ?</a>
@@ -59,7 +63,7 @@ $theContent = get_the_content(); ?>
                     <figure>
                         <?php $image = get_field('membre2'); ?>
                         <img class="img-responsive" src="<?php echo $image['url'];?>" alt="<?php echo $image['title']; ?>">
-                        <figcaption class="name-img"><?php echo $image['caption'] ?></figcaption>
+                        <figcaption class="figcaption cpation-left"><?php echo $image['caption'] ?></figcaption>
                     </figure>
                 </div><!-- ./col-lg-6-->
 
@@ -68,12 +72,13 @@ $theContent = get_the_content(); ?>
 
             <div class="const-1"><!-- Développer des affinités avec vos publics et les engager -->
                 <div class="bloc-text">
-                    <h3><?php echo get_field('title_developper_affinites'); ?></h3>
+                    <h4><?php echo get_field('title_developper_affinites'); ?></h4>
                     <p><?php echo get_field('developper_affinites'); ?></p>
 
                     <div class="plus-infos">
+                        <a class="close-infos-comment"><img src="<?php echo $close; ?>" alt="close"></a>
+                        <p class="comment">Comment ?</p>
                         <p><?php echo get_field('comment_developper_affinites'); ?></p>
-                        <a class="close-infos-comment">Close</a>
                     </div><!-- ./plus-infos -->
 
                     <a class="btn-infos-comment">Comment ?</a>
@@ -83,7 +88,7 @@ $theContent = get_the_content(); ?>
                     <figure>
                         <?php $image = get_field('membre3'); ?>
                         <img class="img-responsive" src="<?php echo $image['url'];?>" alt="<?php echo $image['title']; ?>">
-                        <figcaption class="name-img"><?php echo $image['caption'] ?></figcaption>
+                        <figcaption class="figcaption caption-right"><?php echo $image['caption'] ?></figcaption>
                     </figure>
                 </div><!-- ./col-md-offset-6 col-lg-6 -->
 
@@ -92,12 +97,14 @@ $theContent = get_the_content(); ?>
 
             <div class="const-1"><!-- Déployer en innovant -->
                 <div class="bloc-text-right">
-                    <h3><?php echo get_field('title_deployer_en_innovant'); ?></h3>
+                    <h4><?php echo get_field('title_deployer_en_innovant'); ?></h4>
                     <p><?php echo get_field('deployer_en_innovant'); ?></p>
 
                     <div class="plus-infos">
+                        <a class="close-infos-comment"><img src="<?php echo $close; ?>" alt="close"></a>
+                        <p class="comment">Comment ?</p>
                         <p><?php echo get_field('comment_deployer_en_innovant'); ?></p>
-                        <a class="close-infos-comment">Close</a>
+
                     </div><!-- ./plus-infos -->
 
                     <a class="btn-infos-comment">Comment ?</a>
@@ -107,7 +114,7 @@ $theContent = get_the_content(); ?>
                     <figure>
                         <?php $image = get_field('membre4'); ?>
                         <img class="img-responsive" src="<?php echo $image['url'];?>" alt="<?php echo $image['title']; ?>">
-                        <figcaption class="name-img"><?php echo $image['caption'] ?></figcaption>
+                        <figcaption class="figcaption caption-left"><?php echo $image['caption'] ?></figcaption>
                     </figure>
                 </div><!-- ./col-lg-6-->
 
